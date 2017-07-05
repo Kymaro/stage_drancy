712a2c278d04c6406c59b81bbc0f539c254dae49
Kymaro <eclipse.florian@gmail.com>
2017-07-05 13:52:01 +0200

    nouveau test du screen administrator

--- rpi/test_complet.py
+++ rpi/test_complet.py
@@ -23,14 +23,16 @@ pinMode(led,"OUTPUT")
 pinMode(temp_sensor,"INTPUT")
 pinMode(potentiometer,"INPUT")
 pinMode(lum_sensor,"INTPUT")
+time.sleep(1)
 
 t_refresh = 3000
 t_actuator = 2000
 temp_dht = 0
 hum = 0
 tempe = 0 
-button_value = 1
+mode_value = 0
 lum = 0
+
 
 adc_ref = 5
 grove_vcc = 5
@@ -44,36 +46,52 @@ def DHT() :
     global temp_dht,hum
     [temp_dht,hum] = dht(dht_sensor_port,dht_sensor_type)
 
-def temperature() :
+def Temperature() :
     global tempe
     analog_value = analogRead(temp_sensor)
     R = 1024.0/analog_value - 1.0
     tempe = 1.0/(log(R)/B + 1/298.15) - 273.15
 
+def PotentiometerToDegrees(potentiometer_value) :
+    voltage = round((float)(potentiometer_value)*adc_ref / 1023,2)
+    degrees = round((voltage*full_angle)/grove_vcc,2)
+    return degrees
+
+def screen_administrator() :
+    average_degrees = 0
+    for i in range(10) :
+        value = PotentiometerToDerees(analogRead(potentiometer))
+        average_degrees += value / 10.0
+        time.sleep(1.0/1000.0)
+
+    if (average_degrees <= 100 and average_degrees >= 0) and mode_value != 1: #MODE 1
+        setText("Temperature : \n" +str((tempe + temp_dht)/2.0))
+        setRGB(0,128,255)
+        mode_value = 1
+    elif (average_degrees <= 200 and average_degrees > 100) and mode_value != 2 : #MODE 2
+        setText("Humidite : \n"+str(hum))
+        setRGB(255,0,128)
+        mode_value = 2
+    elif (average_degrees > 200 and average_degrees <=300) and mode_value != 3 : # MODE 3
+        setText("Luminosite \n"+"Bouton :"+str(button_value))
+        setRGB(255,128,0)
+        mode_value = 3   
+
+
+
+
+
 while True :
 
     if ( t_refresh >= t_actuator) : 
-    	temperature()
+    	Temperature()
 	DHT() #A faire en dernier car un delai de retour de valeur digital
     	print(temp_dht)
     	print(hum)
     	print(tempe)
-	t_refresh = 0   
+	t_refresh = 0 
 
-    potentiometer_value = analogRead(potentiometer)
-    voltage = round((float)(potentiometer_value)*adc_ref / 1023,2)
-    degrees = round((voltage*full_angle)/grove_vcc,2)
-
-    if degrees <= 100 and degrees >= 0 :
-        setText("Temperature : \n" +str((tempe + temp_dht)/2.0))
-        setRGB(0,128,255) 	
-    elif degrees <= 200 and degrees > 100 :
-        setText("Humidite : \n"+str(hum))
-        setRGB(255,0,128)
-    else :
-        setText("Luminosite \n"+"Bouton :"+str(button_value))
-        setRGB(255,128,0)
-        
+    screen_administrator()
     time.sleep(50.0/1000.0)
     t_refresh += 100
 
