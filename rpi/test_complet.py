939ee2edd128e39f7ba679c7d881d8168d8142d0
Kymaro <eclipse.florian@gmail.com>
2017-07-17 16:21:19 +0200

    ok

--- rpi/test_complet.py
+++ rpi/test_complet.py
@@ -1,74 +1,45 @@
+##############################################IMPORTATION DES BIBLIOTHEQUES##############################################
 from grovepi import *
 from grove_rgb_lcd import *
 import time,datetime,json
 from math import *
-from azure.servicebus import ServiceBusService
-from azure.servicebus import Message
-
-
-dht_sensor_port = 7
-dht_sensor_type = 0
-
-temp_sensor = 0
-lum_sensor = 1
-potentiometer = 2
-
-button = 3
-led = 4
-
-B=4250
-R0 = 100000
-
-pinMode(button,"INTPUT")
-pinMode(led,"OUTPUT")
-pinMode(temp_sensor,"INTPUT")
+#from azure.servicebus import ServiceBusService
+#from azure.servicebus import Message
+###########################################DECLARATION DES CONSTANTES GLOBALES###########################################
+dht_sensor_port = 7 #capteur humidite et temperature (DHT11 ou DHT22) sur D7
+dht_sensor_type = 0 #mettre 0 si bleu (DHT11) ou 1 si blanc (DHT22)
+t_actuator = 799 #nombre de loop entre deux envoie de donnees
+t_wait = 25 
+lum_seuil = 10 #seuil au dessus du quel on determine la lumiere comme allume
+lum_sensor = 1 #capteur de lumiere sur A1
+potentiometer = 2 #bouton de menu sur A2
+##########################################ACTIVATION DES PORTS DU SHIELD GROVEPI#########################################
 pinMode(potentiometer,"INPUT")
 pinMode(lum_sensor,"INTPUT")
-pinMode(4,"OUTPUT")
 time.sleep(1)
-
-t_refresh = 6000
-t_actuator = 799
-t_wait = 50
-compteur_echec_envoie = 0
-
+####################################################VARIABLES INTERNES###################################################
+t_refresh = 800 # nombre de loop entre deux envoie a l instant t
+compteur_echec_envoie = 0 # compteur d echec d envoie de donnee sur azure (se remet a 0 si reussite)
+mode_value = 0 # les deux valeurs permette d eviter une actualisation inutile de l ecran qui le faisait clignoter
+mode_value_old = 0
+##############################################VARIABLES DE DONNEES ENVOYEES##############################################
 temp_dht = 0
 hum = 0
-tempe = 0 
-mode_value = 0
-mode_value_old = 0
 lum = 0
-
-
-adc_ref = 5
-grove_vcc = 5
-full_angle = 300
-lum_seuil = 10
-
-setText("Bienvenue\ndans l'IoT Hub")
-setRGB(128,255,0)
-time.sleep(2)
-
-def DHT() : #temperature et humidite analogique
+identifiant = "Rpi Test" # a modifier
+########################################################FONCTIONS########################################################
+def DHT() : #temperature et humidite numerique
     global temp_dht,hum
     [temp_dht,hum] = dht(dht_sensor_port,dht_sensor_type)
     temp_dht = round(temp_dht,1)
-def Temperature() : #capteur de temperature analogique
-    global tempe
-    analog_value = analogRead(temp_sensor)
-    tempe = (((analog_value *5000.0)/1024.0) - 500.0) / 10.0
-    #R = 1023.0/analog_value - 1.0
-    #tempe = 1.0/(log(R)/B + 1/298.15) - 273.15
-    tempe= round(tempe,1)
 
+def Luminosite() : #luminosite qui envoie True ou False avec le seuil
+    lum_value = analogRead(lum_sensor)
 
-def Luminosite() :
-	lum_value = analogRead(lum_sensor)
-
-def screen_administrator() : # permet de gerer lecran sans quil refresh a chaque iteration 
+def screen_administrator(encoder) : # permet de gerer lecran sans quil refresh a chaque iteration 
     global mode_value
-    encoder_value = analogRead(potentiometer)
     mode_value_old = mode_value
+    print(encoder_value)
     if (encoder_value <=341 and encoder_value >= 0) and mode_value != 1 : #MODE 1
        	setText("Temperature : \n" +str(temp_dht))
        	setRGB(0,128,255)
@@ -83,64 +54,41 @@ def screen_administrator() : # permet de gerer lecran sans quil refresh a chaque
        	mode_value = 3   
     if not ((mode_value - mode_value_old) != 0) : #s il y a pas eu un changement de mode sur l ecran
         time.sleep(140.0/1000.0) #on attend 140 ms pour etre sur du temps a chaque loop
-
-def createSBS() : 
+"""
+def createSBS() : #permet de creer le canal de communication avec Azure 
 	
-	service_namespace = 'RaspberryPiNSTest'
-	key_name = 'RootManageSharedAccessKey'
-	key_value = 'yEcs0927kFYs1U8J7x4VCwZAaf3ck38hqSGY9YjiMAo='
+    service_namespace = 'RaspberryPiNSTest' #a modifier
+    key_name = 'RootManageSharedAccessKey' # a modifier
+    key_value = 'yEcs0927kFYs1U8J7x4VCwZAaf3ck38hqSGY9YjiMAo=' # a modifier
 	
-	sbs = ServiceBusService(service_namespace, shared_access_key_name=key_name, shared_access_key_value=key_value)
+    sbs = ServiceBusService(service_namespace, shared_access_key_name=key_name, shared_access_key_value=key_value)
 
-	return sbs
-
-sbs = createSBS()
-
+    return sbs
+"""
+##########################################################SETUP##########################################################
+#sbs = createSBS()
+setText("Bienvenue\ndans l'IoT Hub")
+setRGB(128,255,0)
+time.sleep(2)
+######################################################BOUCLE INFINI######################################################
 while True :
     if ( t_refresh >= t_actuator) : 
-
-    	#Temperature()
-	#time.sleep(500.0/1000.0)
-	#Luminosite()
-	DHT() #A faire en dernier car un delai de retour de valeur digital
-	while (isnan(temp_dht) or temp_dht == 0) : #si jamais le capteur foire au releve
-		DHT()
-    	#print(temp_dht)
-    	#print(hum)
-    	#print(tempe)
-        average_temp = temp_dht
-	average_temp = round(average_temp,1)
-	t_refresh = 10 
-	identifiant = "Rpi Test"
+	DHT()
+	while (isnan(temp_dht) or temp_dht == 0) : # on essaie tant que le capteur n a pas de valeur valide
+	    DHT()
         dt = str(datetime.datetime.now())
-        d = {'DeviceID' : identifiant, 'Temperature' : average_temp, 'Humidity' : hum,'Time' : dt }
-        msg = json.dumps(d)
-        #print(msg)
+        d = {'DeviceID' : identifiant, 'Temperature' : temp_dht, 'Humidity' : hum,'Time' : dt }
+        msg = json.dumps(d) #cree le message a envoyer
         try :
-            sbs.send_event('dht11',msg)
+            #sbs.send_event('dht11',msg) #a modifier
             compteur_echec_envoie = 0
         except :
             compteur_echec_envoie += 1
             if compteur_echec_envoie == 720 : #echec d envoie de message depuis 24h
                 setRGB(255,0,0)
 		setText("Probleme envoie\nmessage azure")
-		break
-    if (t_refresh >= t_wait) : # on attend un peu avant de refresh l ecran car valeur aberante de l encoder quand on regarde les autres capteur
-        screen_administrator()
-	#Luminosite()
+		break # sort de la boucle, reboot necessaire du programme ou de la RPI
+    if (t_refresh >= t_wait) : # on attend un peu avant de refresh l ecran
+        encoder_value = analogRead(potentiometer)
+        screen_administrator(encoder_value)
     t_refresh += 1
-
-"""
-https://docs.microsoft.com/fr-fr/azure/iot-hub/iot-hub-python-getstarted
-http://www.stevenfowler.me/p/send-raspberry-pi-data-azuure/
-http://www.instructables.com/id/Sending-Temperature-Sensor-Data-to-Azure-Database/
-
-login azure cli : "azure login"
-
-temps d'excecution des loop :
-
-    si actualisation : 1 loop = 450ms
-    sinon : 150 ms
-
-    
-"""
